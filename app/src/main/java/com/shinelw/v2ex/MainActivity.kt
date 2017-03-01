108bea8b6e152fbf8f735bff7d2f210ce39dc032
ShenLiangWei <shinelw.cool@gmail.com>
2017-03-01 10:54:30 +0800

    First commit

---
+++ app/src/main/java/com/shinelw/v2ex/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.shinelw.v2ex
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
